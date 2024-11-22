5e4626bbf7e6b05d0b1fc2cbff7b70867b68c953
wangxu <xu.wang@newtouch.com>
2024-11-22 14:05:12 +0800

    jupyter housing

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import kaggle
+import kagglehub
+
+api = kaggle.api
 
 def main():
-    print('hello')
+    # Download latest version
+    path = kagglehub.dataset_download("camnugent/california-housing-prices")    
+    print("Path to dataset files:", path)
 
 if __name__ == '__main__':
     main()
