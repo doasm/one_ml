6139cf773073ba0d182ee204ebe55e82782b279e
wangxu <xu.wang@newtouch.com>
2025-03-10 15:14:45 +0800

    download dataset

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import kaggle
+from pathlib import Path
+import shutil
 import kagglehub
 
-api = kaggle.api
-
 def main():
-    # Download latest version
-    path = kagglehub.dataset_download("camnugent/california-housing-prices")    
-    print("Path to dataset files:", path)
+    # Download dataset
+    data_set_name = 'camnugent/california-housing-prices'
+    data_set_path = kagglehub.dataset_download(data_set_name)    
+    src_path = Path(data_set_path)
+    dest_path = Path('./kaggle') / data_set_name
+    shutil.copytree(src_path, dest_path)
 
 if __name__ == '__main__':
     main()
